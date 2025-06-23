0d56fdd42cbc7ffba39c83cb512cd1b1ab335ffd
BasantRana <basantranaji2020@gmail.com>
2025-06-23 06:04:39 +0000

    constLetVar

---
+++ 01_Basics/constLetVar.js
@@ -0,0 +1,19 @@
+const accountId = 8823209008
+let accountEmail = "basantranaji2020@gmail.accountEmail"
+var accountPassword = "589645"
+let state = undefined;
+let accountState;
+accountCity = "Sambhal"
+
+//accountId = 2   ---- not allowed
+
+accountEmail = "basantkumar2023@gmail.com"
+accountPassword = "156324"
+accountCity = "Gowan"
+
+// Prefer not to use var , because of isssue in block scope and functional scope
+
+
+
+console.table([accountId,accountEmail, accountPassword,accountCity,state,accountState]);
+
